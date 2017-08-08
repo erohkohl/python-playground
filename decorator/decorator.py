0a4c2e12ebea5705c7c07a7d18a0465c9b78feb2
erik_rohkohl <erikrohkohl@googlemail.com>
2017-08-08 14:32:19 +0200

    Decorator: DONE

--- decorator/decorator.py
+++ decorator/decorator.py
@@ -1,14 +1,19 @@
 import time
 
-#def measure_time_of_func(func):
+# Decorator
+def measure_time_of_func(func):
+    def wrapper_func(n):
+        start_time = time.time()
+        fib_seq = func(n)
+        end_time = time.time()
+        return (fib_seq, end_time - start_time)
+    return wrapper_func
 
-#	def wrapper_func(*agrs, **kwargs)
-	
+
 # Returns a list with first n numbers of fibonacci sequence.
+@measure_time_of_func
 def fib(n):
-	sequence = [1, 1]
-	for i in range(2, n, 1):
-		sequence.append(sequence[i - 1] + sequence[i - 2])
-	return sequence
-
-
+    sequence = [1, 1]
+    for i in range(2, n, 1):
+        sequence.append(sequence[i - 1] + sequence[i - 2])
+    return sequence
--- decorator/test_decorator.py
+++ decorator/test_decorator.py
@@ -2,6 +2,13 @@ import decorator as Decorator
 
 
 def test_fib():
-    result = Decorator.fib(10)
-    expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 51]
+    (result, _) = Decorator.fib(10)
+    expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+
     assert expected == result
+
+
+def test_fib_time():
+    (_, time) = Decorator.fib(10)
+
+    assert time > 0
